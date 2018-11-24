b4a611c194be840c273b5e70d586ec3e1c4e9f79
Krzysztof Pawlowski <kpawlowski1976@gmail.com>
2018-11-25 00:53:10 +0100

    Updates

---
+++ precache-manifest.3ce8598e60a566dc751b7e8d2cbd97cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "19d4759b2a148cc265e3",
+    "url": "/paper-scissors-rock/static/js/runtime~main.19d4759b.js"
+  },
+  {
+    "revision": "2ba013c139813256b7af",
+    "url": "/paper-scissors-rock/static/js/main.2ba013c1.chunk.js"
+  },
+  {
+    "revision": "f68dcc1a0c0c6e129be0",
+    "url": "/paper-scissors-rock/static/js/1.f68dcc1a.chunk.js"
+  },
+  {
+    "revision": "2ba013c139813256b7af",
+    "url": "/paper-scissors-rock/static/css/main.673d8851.chunk.css"
+  },
+  {
+    "revision": "d0ab0f3a54e80485b79eb3aea8c23e37",
+    "url": "/paper-scissors-rock/index.html"
+  }
+];
