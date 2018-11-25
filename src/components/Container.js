344d5206015fec1a839417a81c7ddcef2421cd36
Krzysztof Pawlowski <kpawlowski1976@gmail.com>
2018-11-25 12:25:15 +0100

    Add basic styles and Container component

---
+++ src/components/Container.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Container extends Component {
+	render() {
+		return (
+			<div className="container">
+				<h1 className="title">papier, nożyczki, kamień</h1>
+			</div>
+		);
+	}
+}
+
+export default Container;
