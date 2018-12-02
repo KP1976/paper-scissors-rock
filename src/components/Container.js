17301436be87de5ff5288119d8cccfcaec790257
Krzysztof Pawlowski <kpawlowski1976@gmail.com>
2018-12-02 11:55:24 +0100

    Change Container and Game components

--- src/components/Container.js
+++ src/components/Container.js
@@ -6,24 +6,48 @@ import scissors from '../images/nozyczki.svg';
 import rock from '../images/kamien.svg';
 
 class Container extends Component {
+	state = {
+		imageIndex: 0,
+	};
+
+	indexChoice = e => {
+		switch (e.target.firstElementChild.getAttribute('alt')) {
+			case 'paper': {
+				this.setState({ imageIndex: 0 });
+				break;
+			}
+			case 'scissors': {
+				this.setState({ imageIndex: 1 });
+				break;
+			}
+			case 'rock': {
+				this.setState({ imageIndex: 2 });
+				break;
+			}
+			default: {
+				break;
+			}
+		}
+	};
+
 	render() {
 		return (
 			<div className="container">
 				<header className="header">
 					<h1 className="header__title">papier, nożyczki, kamień</h1>
 					<figure className="images">
-						<div className="images__circle">
-							<img className="images__img" src={paper} alt="paper SVG" />
+						<div className="images__circle" onClick={this.indexChoice}>
+							<img className="images__img" src={paper} alt="paper" />
 						</div>
-						<div className="images__circle">
-							<img className="images__img" src={scissors} alt="scissors SVG" />
+						<div className="images__circle" onClick={this.indexChoice}>
+							<img className="images__img" src={scissors} alt="scissors" />
 						</div>
-						<div className="images__circle">
-							<img className="images__img" src={rock} alt="rock SVG" />
+						<div className="images__circle" onClick={this.indexChoice}>
+							<img className="images__img" src={rock} alt="rock" />
 						</div>
 					</figure>
 				</header>
-				<Game />
+				<Game choice={this.state.imageIndex} />
 			</div>
 		);
 	}
--- src/components/Game.js
+++ src/components/Game.js
@@ -6,9 +6,9 @@ import rock from '../images/kamien.svg';
 
 class Game extends Component {
 	state = {
-		hands: [rock, paper, scissors],
+		hands: [paper, scissors, rock],
 		playerIndex: 0,
-		computerIndex: 0,
+		computerIndex: Math.floor(Math.random() * 3),
 		resultIndex: 0,
 		playerScore: 0,
 		computerScore: 0,
@@ -16,21 +16,27 @@ class Game extends Component {
 	};
 
 	play = () => {
-		const playerChoice = Math.floor(Math.random() * 3);
+		const playerChoice = this.props.choice;
 		const computerChoice = Math.floor(Math.random() * 3);
 
 		if (playerChoice === computerChoice) {
 			this.setState({ resultIndex: 0 });
+			// Papier - Nożyczki
 		} else if (playerChoice === 0 && computerChoice === 1) {
 			this.setState({ resultIndex: 2, computerScore: this.state.computerScore + 1 });
+			// Papier - Kamień
 		} else if (playerChoice === 0 && computerChoice === 2) {
 			this.setState({ resultIndex: 1, playerScore: this.state.playerScore + 1 });
+			// Nożyczki - Papier
 		} else if (playerChoice === 1 && computerChoice === 0) {
 			this.setState({ resultIndex: 1, playerScore: this.state.playerScore + 1 });
+			// Nożyczki - Kamień
 		} else if (playerChoice === 1 && computerChoice === 2) {
 			this.setState({ resultIndex: 2, computerScore: this.state.computerScore + 1 });
+			// Kamień - Papier
 		} else if (playerChoice === 2 && computerChoice === 0) {
 			this.setState({ resultIndex: 2, computerScore: this.state.computerScore + 1 });
+			// Kamień - Nożyczki
 		} else if (playerChoice === 2 && computerChoice === 1) {
 			this.setState({ resultIndex: 1, playerScore: this.state.playerScore + 1 });
 		}
@@ -39,20 +45,16 @@ class Game extends Component {
 	};
 
 	render() {
-		const {
-			hands,
-			playerIndex,
-			computerIndex,
-			resultIndex,
-			playerScore,
-			computerScore,
-			gameResult,
-		} = this.state;
+		const { hands, computerIndex, resultIndex, playerScore, computerScore, gameResult } = this.state;
 
 		return (
 			<React.Fragment>
 				<main className="game">
-					<img className="game__player-hand" src={hands[playerIndex]} alt="paper, scissors or rock SVG" />
+					<img
+						className="game__player-hand"
+						src={hands[this.props.choice]}
+						alt="paper, scissors or rock SVG"
+					/>
 					<div className="game__result">{gameResult[resultIndex]}</div>
 					<img className="game__computer-hand" src={hands[computerIndex]} alt="paper, scissors or rock SVG" />
 					<span className="game__player-text">Gracz</span>
@@ -62,8 +64,8 @@ class Game extends Component {
 					<span className="game__computer-text">Komputer</span>
 				</main>
 				<div className="result">
-					<h2 className="result__title">Wynik:</h2>
 					<span className="result__player">{playerScore}</span>
+					<h2 className="result__title">Wynik</h2>
 					<span className="result__computer">{computerScore}</span>
 				</div>
 			</React.Fragment>
